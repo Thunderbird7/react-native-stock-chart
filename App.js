93118df9f4aff4a51ca60bda0c5669ec1901ac78
prcbass <cbasshernandez@gmail.com>
2018-06-08 17:43:08 -0400

    Add test data and correct build dependencies

--- App.js
+++ App.js
@@ -7,28 +7,60 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import data from './data';
 
-type Props = {};
-export default class App extends Component<Props> {
+console.log(data);
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+const CHART_HEIGHT_PIXELS = 260;
+
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>
+      //     Welcome to React Native!
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     To get started, edit App.js
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     {instructions}
+      //   </Text>
+      // </View>
+      <VictoryGroup
+        containerComponent={
+          <VictoryVoronoiContainer
+            voronoiDimension="x"
+            labels={() => {}}
+          />
+        }
+        padding={10}
+      >
+        <VictoryLine
+          style={{
+            data: {
+              stroke: 'black',
+              strokeWidth: 2,
+            },
+            parent: {
+              marginLeft: 0,
+            },
+          }}
+          padding={{
+            top: 50,
+            bottom: 10,
+          }}
+          height={CHART_HEIGHT_PIXELS}
+          data={data}
+        />
+      </VictoryGroup>
     );
   }
 }
